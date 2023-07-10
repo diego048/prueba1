7fdc3d21dccbe524d735d607582051cf2e4745ae
diego048 <103468763+diego048@users.noreply.github.com>
2023-07-10 14:32:59 -0500

    Create about.vue

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <form name="contact1" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+        <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
