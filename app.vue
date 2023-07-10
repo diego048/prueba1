6fa6b35da2782a00fa252c6515d94a8ef5934b23
diego048 <103468763+diego048@users.noreply.github.com>
2023-07-10 13:53:07 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <div>
     <form name="ask-question" netlify netlify-honeypot="bot-field">
       <input type="text" name="name" />
+      <button>Submit</button>
     </form>
   </div>
 </template>
