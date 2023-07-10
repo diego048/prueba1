9ecf21d25b2f7e7c5ae069fab3b10b1331047778
diego048 <103468763+diego048@users.noreply.github.com>
2023-07-10 14:14:42 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
-    <form name="ask-question" method="POST" netlify netlify-honeypot="bot-field" onSubmit="submit">
-      <input type="text" name="name" />
-      <button type="submit">Submit</button>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   </div>
 </template>
